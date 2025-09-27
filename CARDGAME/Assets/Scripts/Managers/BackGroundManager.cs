5f56f2ca8b560cc953fcb9f16742819026ffd78f
ConnorRussi <ConnorARussi@Gmail.com>
2025-09-27 01:52:40 -0500

    begun the background backbone. NEED to set up initial spawning but have beign adn end set up

---
+++ CARDGAME/Assets/Scripts/Managers/BackGroundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundManager : MonoBehaviour
+{
+    bool scrolling = false;
+    public float scrollSpeed = 0.5f;
+    public GameObject background1;
+    public Transform background1StartPos;
+    public Transform background1EndPos;
+    public List<GameObject> backgrounds;
+    void StartScrolling()
+    {
+        while (scrolling)
+        {
+            
+        }
+    }
+}
